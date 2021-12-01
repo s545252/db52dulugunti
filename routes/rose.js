2b69deb752a229440fa6951ae569418de11e54ea
s545252 <s545252@nwmissouri.edu>
2021-12-01 16:29:29 -0600

    authentication failure

--- routes/rose.js
+++ routes/rose.js
@@ -1,10 +1,18 @@
 var express = require('express'); 
 const rose_controller= require('../controllers/rose'); 
 var router = express.Router(); 
- 
+
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
+
 /* GET create rose page */ 
 //router.get('/delete', rose_controller.rose_delete_Page); 
 router.get('/', rose_controller.rose_view_all_Page);
-router.get('/update', rose_controller.rose_update_Page);
+router.get('/update', secured, rose_controller.rose_update_Page); 
 
 module.exports = router;
